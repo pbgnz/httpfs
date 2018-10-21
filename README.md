8c7e45e02f620e73acf9aeac4fa57a0ac589a7ba
pbgnz <pabgonza1995@gmail.com>
2018-10-21 14:36:33 -0400

    implement the server and start handling the request

--- README.md
+++ README.md
@@ -8,11 +8,40 @@ httpfs is a simple file server
 
 ### General
 
+#### server
+
 ``` bash
 httpfs is a simple file server.
 
 usage: httpfs [-v] [-p PORT] [-d PATH-TO-DIR]
     -v Prints debugging messages.
-    -p Specifies the port number that the server will listen and serve at. Default is 8080.
-    -d Specifies the directory that the server will use to read/write requested files. Default is the current directory when launching the application.
+    -p Specifies the port number that the server will listen and serve at. 
+    Default is 8080.
+    -d Specifies the directory that the server will use to read/write requested files. 
+    Default is the current directory when launching the application.
+```
+
+#### client
+
+GET / returns a list of the current files in the data directory.
+
+``` bash
+curl -get localhost:8080/
+
+httpc -p 8080 get 'http://localhost:8080/'
+```
+
+GET /foo.txt returns the content of the file named foo.txt in the data directory.
+``` bash
+curl -get localhost:8080/foo.txt
+
+httpc -v -p 8080 get 'http://localhost:8080/foo.txt'
+```
+
+POST /bar should create or overwrite the file named bar in the data directory with
+the content of the body of the request.
+``` bash
+curl -post -d "foo" localhost:8080/bar.txt
+
+httpc -p 8080 -h Content-Type:application/text -d 'foo' post 'http://localhost:8080/bar.txt'
 ```
--- httpfs.go
+++ httpfs.go
@@ -1,50 +1,75 @@
 package main
 
 import (
-	"encoding/binary"
+	"bufio"
 	"flag"
 	"fmt"
+	"log"
 	"net"
 	"os"
-	"time"
+)
+
+var (
+	verbose = flag.Bool("v", false, "Prints debugging messages.")
+	port    = flag.Int("p", 8080, "Specifies the port number that the server will listen and serve at.")
+	// directory = flag.String("d", "", "Specifies the directory that the server will use to read/write requested files.")
 )
 
 func main() {
-	port := flag.Int("port", 8037, "time server port")
 	flag.Parse()
+
 	addr := fmt.Sprintf(":%d", *port)
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to listen %s with %v\n", addr, err)
+		fmt.Fprintf(os.Stderr, "Error: failed to listen %s with %v\n", addr, err)
 		return
 	}
 
 	defer listener.Close()
-	fmt.Println("Time server is listening at", listener.Addr())
+	fmt.Println("httpfs is listening at", listener.Addr())
 
+	// connection-loop:
+	// handle incomming requests
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Printf("failed to accept connection %v\n", err)
+			fmt.Println(err)
+			if err := conn.Close(); err != nil {
+				log.Println("Error: failed to close listener:", err)
+			}
 			continue
 		}
-		go func(conn net.Conn) {
-			defer conn.Close()
-			if err := reportTime(conn); err != nil {
-				fmt.Fprintf(os.Stderr, "failed to report time %v\n", err)
-			}
-		}(conn)
+
+		if *verbose {
+			log.Println("Connected to", conn.RemoteAddr())
+		}
+
+		go handleConnection(conn)
 	}
 }
 
-func reportTime(conn net.Conn) error {
-	// Number of seconds elapsed from 1900 to 1970
-	var time1970 int64 = 2208988800
-	now := time.Now().Unix() + time1970
-	buf := make([]byte, 4)
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
 
-	// Must send the uint32 in big-endian over the network
-	binary.BigEndian.PutUint32(buf, uint32(now))
-	_, err := conn.Write(buf)
-	return err
+	req, err := handleRequest(conn)
+	if err != nil {
+		log.Printf("Error reading the request: %v", err)
+		return
+	}
+	log.Println(req)
+}
+
+func handleRequest(conn net.Conn) (*Request, error) {
+	defer conn.Close()
+
+	requestLine, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		return nil, fmt.Errorf("Error in the request line: %v", err)
+	}
+
+	if *verbose {
+		fmt.Println(requestLine)
+	}
+
+	return nil, nil
 }
